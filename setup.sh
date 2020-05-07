faa4fece81a97cee646a66426b93f4759f8221db
danielpeng2 <danielpeng2@gmail.com>
2020-05-07 14:01:58 -0400

    zathura config for clipboard

--- setup.sh
+++ setup.sh
@@ -2,28 +2,25 @@
 
 # vim
 ln -fsv $PWD/.vimrc ~/
-
-# coc
-ln -fsv $PWD/.config/coc/coc-settings.json ~/.vim/
-
 # zsh
 ln -fsv $PWD/.zshrc ~/
-
-# alacritty
-ln -fsv $PWD/.config/alacritty/alacritty.yml ~/.config/alacritty
-
 # tmux
 ln -fsv $PWD/.tmux.conf ~/
 
-# scripts
-ln -fsv $PWD/.local/bin/* /usr/local/bin
-
+# coc
+ln -fsv $PWD/.config/coc/coc-settings.json ~/.vim/
+# alacritty
+ln -fsv $PWD/.config/alacritty/alacritty.yml ~/.config/alacritty
 # i3
 ln -fsv $PWD/.config/i3/config ~/.config/i3/config
-
 # i3blocks
 ln -fsv $PWD/.config/i3blocks/config ~/.config/i3blocks/config
 ln -fsv $PWD/.config/i3blocks/i3blocks_scripts ~/.config/i3blocks
+# zathura
+ln -fsv $PWD/.config/zathura/zathurarc ~/.config/zathura
+
+# scripts
+ln -fsv $PWD/.local/bin/* /usr/local/bin
 
 # lockscreen
 ln -fsv $PWD/.config/i3/i3lock.service /etc/systemd/system
